ba7f81cfd1acf77916d966dcfc250d24f44adbb9
gavirttt <158991290+gavirttt@users.noreply.github.com>
2024-05-01 13:06:40 +0800

    initial commit

---
+++ my_app/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:my_app/screen/mycart.dart';
+import 'package:my_app/screen/mycatalog.dart';
+import 'provider/shoppingcart_provider.dart';
+
+// multiprovider from provider package
+void main() {
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context) => ShoppingCart()),
+  ], child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/cart": (context) => const MyCart(), // page route for cart page
+        "/products": (context) => const MyCatalog(), // page route for catalog page
+      },
+      home: const MyCatalog(),
+    );
+  }
+}
